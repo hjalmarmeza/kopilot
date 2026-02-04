a3efb4959243f386db2d3b9a970be7ba957ba264
Hjalmar Meza <hjalmar.meza@gmail.com>
2026-02-04 22:07:23 +0100

    Kopilot v8.0: Delete Single Log & Improved Labels

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'kopilot-flow-v7.0';
+const CACHE = 'kopilot-v8.0-final';
 const FILES = [
-    './index.html?v=7.0',
-    './style.css?v=7.0',
-    './app.js?v=7.0',
+    './index.html?v=8.0',
+    './style.css?v=8.0',
+    './app.js?v=8.0',
     './manifest.json',
     'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap',
     'https://fonts.googleapis.com/icon?family=Material+Icons+Round'
